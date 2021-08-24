db5c80a1d60ce61545faa92b51aabf71ad17e45f
SDMourtas <75614853+SDMourtas@users.noreply.github.com>
2021-08-24 21:24:20 +0300

    Add files via upload

--- Main_MI_BASWASDN.m
+++ Main_MI_BASWASDN.m
@@ -23,13 +23,15 @@ clc
 
 % Choose modeling problem (for x = 1 to 4)
 x=1;
-% Choose method: BASWASD (y = 1) or MIHPN_WASD (y = 2)
+% Choose method: BASWASD (y = 1), MIHPN_WASD (y = 2)
+%                Linear SVM (y==3), Exp. GPR (y==4)
 y=1;
 
 [X_train,Y_train,X_test,Y_test,p,d,delta,n,tmax]=problem(x);
 [H,M]=size(X_train);
 
 %% Training
+if y==1 || y==2
 % Data Preprocessing - Normalization
 [z_N,z_min,z_max]=Normalization(X_train); 
 [y_N,y_min,y_max]=Normalization(Y_train);
@@ -48,6 +50,19 @@ end
 %% Predict
 predt=predictN(X_train,W,N,z_min,z_max,y_min,y_max,y);
 pred=predictN(X_test,W,N,z_min,z_max,y_min,y_max,y);
+elseif y==3
+tic
+trainedModel=LSVM([X_train,Y_train],x);
+toc
+predt=trainedModel.predictFcn(X_train);
+pred=trainedModel.predictFcn(X_test);
+elseif y==4
+tic
+trainedModel=EGPR([X_train,Y_train],x);
+toc
+predt=trainedModel.predictFcn(X_train);
+pred=trainedModel.predictFcn(X_test);
+end
 
 Et=error_pred(predt,Y_train,pred,Y_test); % Error of test data
 
@@ -62,6 +77,7 @@ xlabel('Test Data Samples');ylabel('Price')
 legend('Predicted price','Actual price')
 hold off
 
+if y==1 || y==2
 figure
 semilogy(0:length(E)-1,E,'Color',[0.4940 0.1840 0.5560])
 hold on
@@ -74,3 +90,4 @@ elseif y==2
 end
 legend('$E_N$','$E_{N^*}$','Interpreter','latex')
 hold off
+end
