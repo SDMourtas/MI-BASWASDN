a5f8871cfd1d3220f64780a548d1b874a2921d9c
SDMourtas <75614853+SDMourtas@users.noreply.github.com>
2021-08-31 21:48:06 +0300

    Update Main_MI_BASWASDN.m

--- Main_MI_BASWASDN.m
+++ Main_MI_BASWASDN.m
@@ -11,9 +11,9 @@
 %           spirosmourtas@gmail.com                                 %
 %                                                                   %
 %   Main paper: T.E. Simos, S.D.Mourtas, V.N.Katsikis,              %
-%               "Multi-Input Beetle Antennae Search WASD Neuronet   %
-%                with Applications in European Central Bank         %
-%                Publications",(submitted)                          %
+%               "Multi-Input Bio-inspired Weights and Structure     %
+%               Determination Neuronet with Applications in         %
+%               in European Central Bank Publications",(submitted)  %
 %                                                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
